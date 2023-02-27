b2fc39f39ca8090378be6857ecd03f5a3ddc847c
jpedro85 <pedro.games30@gmail.com>
2023-02-27 19:33:49 +0000

    initial test

---
+++ Proj1/test.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:32:42 02/27/2023 
+-- Design Name: 
+-- Module Name:    test - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           Res : out  STD_LOGIC);
+end test;
+
+architecture Behavioral of test is
+
+begin
+	process(a,b)
+		begin
+			Res<=(a and b);
+		end process;
+
+end Behavioral;
+
